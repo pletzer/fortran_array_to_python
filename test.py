d7b7afd387171ce5bbee4e8a71310be7781ed3c8
Alex Pletzer <alexander@gokliya.net>
2022-01-28 15:52:15 +1300

    code simplification

--- test.py
+++ test.py
@@ -3,18 +3,18 @@ lib = ctypes.CDLL('libflibex.dylib')
 
 lib.init_()
 
-ptr = ctypes.c_void_p(0)
+ptr = ctypes.POINTER(ctypes.c_float)()
 lib.getpointer_(ctypes.byref(ptr))
 print(ptr)
-
-# cast
-dary = ctypes.cast(ptr, ctypes.POINTER(ctypes.c_float))
 
 import numpy
 
 # create a numpy array that shares the same address as the fortran array
-ary = numpy.ctypeslib.as_array(dary, shape=(20, 10))
-print(ary)
+# be aware that the dimension ordering is different between fortran and python
+ary = numpy.ctypeslib.as_array(ptr, shape=(20, 10))
+print(ary.flags)
 
+del ary
 
-
+ary2 = numpy.ctypeslib.as_array(ptr, shape=(20, 10))
+print(ary2)
